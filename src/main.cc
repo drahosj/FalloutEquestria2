91f053ad2ccf4d10cc35589bb5b8daa8ce92532c
Jake Drahos <drahos.jake@gmail.com>
2013-06-17 19:36:52 -0500

    Put proper boiledplate on main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,26 @@
 /*
- ============================================================================
- Name        : FalloutEquestria2.cpp
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C++,
- ============================================================================
+ * main.cc
+ *
+ *  Created on: May 24, 2013
+ *      Author: jake
+ *
+ *  Copyright (C) 2013  Jake Drahos
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 
 #include <iostream>
 #include <SDL/SDL.h>
@@ -27,6 +41,7 @@ int main() {
 	return 0;
 }
 
-void WinMain() { //fuck this shit
+int WinMain() { //fuck this shit
 	main();
+	return 0;
 }
